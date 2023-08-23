67c15076df5cbd8e6d615ae94fed46a202642826
Laura <law7676@outlook.com>
2023-08-23 12:21:50 -0500

    added setColors and functionality to remove colors from UI

--- src/Color.js
+++ src/Color.js
@@ -1,9 +1,13 @@
 import StarRating from './StarRating'
+import { FaTrash } from 'react-icons/fa'
 
-const Color = ({ title, color, rating }) => {
+const Color = ({ id, title, color, rating, onRemove = (f) => f }) => {
   return (
     <section>
       <h1>{title}</h1>
+      <button style={{ marginBottom: '2px' }} onClick={() => onRemove(id)}>
+        <FaTrash />
+      </button>
       <div style={{ height: 50, backgroundColor: color }} />
       <StarRating selectedStars={rating} />
     </section>
