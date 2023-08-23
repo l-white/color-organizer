70014edd6baf38c392cc3e007a15a38af3c2d792
Laura <law7676@outlook.com>
2023-08-23 13:31:11 -0500

    added color form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import { useState } from 'react'
 import colorData from './color-data.json'
 import ColorList from './ColorList'
+import AddColorForm from './AddColorForm'
+import { v4 } from 'uuid'
 import './App.css'
 
 const App = () => {
   const [colors, setColors] = useState(colorData)
   return (
-    <ColorList
-      colors={colors}
-      onRemoveColor={(id) => {
-        const newColors = colors.filter((color) => color.id !== id)
-        setColors(newColors)
-      }}
-    />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color,
+            },
+          ]
+          setColors(newColors)
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveColor={(id) => {
+          const newColors = colors.filter((color) => color.id !== id)
+          setColors(newColors)
+        }}
+      />
+    </>
   )
 }
 
